34b5ea77d1b743ebf86cd3f11794b340be39ea58
FnlTochka <me@fnltochka.ru>
2026-05-03 01:03:46 +0300

    nix: drop cache.nixos-cuda substituter

--- modules/nixos/system/nix.nix
+++ modules/nixos/system/nix.nix
@@ -65,7 +65,6 @@ in
           auto-optimise-store = true;
           substituters = [
             "https://cache.nixos.org"
-            "https://cache.nixos-cuda.org"
             "https://nix-community.cachix.org"
             "https://cuda-maintainers.cachix.org"
             "https://cache.garnix.io"
@@ -73,7 +72,6 @@ in
           ];
           trusted-public-keys = [
             "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-            "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
             "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
             "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
             "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
